6c256b56609d18686236616506f692b14ab2b7d3
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-09-08 15:06:56 +0200

    Remove the default `app` module

--- app/src/commonMain/kotlin/org/example/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.example
-
-expect class Platform {
-    constructor()
-
-    val name: String
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ plugins {
 
 rootProject.name = "DevToolkt"
 
-include("app")
 include("core")
 include("coreTestUtils")
 include("jsApiCompat")
