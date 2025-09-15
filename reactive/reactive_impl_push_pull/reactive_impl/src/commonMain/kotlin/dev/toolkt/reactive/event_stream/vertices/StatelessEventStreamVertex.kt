f80721b63b9f2881248d768314dc248a8530b90e
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-09-15 15:46:36 +0200

    Rename `stabilizeState` to `stabilize`

--- reactive/reactive_impl_push_pull/reactive_impl/src/commonMain/kotlin/dev/toolkt/reactive/event_stream/vertices/StatelessEventStreamVertex.kt
+++ reactive/reactive_impl_push_pull/reactive_impl/src/commonMain/kotlin/dev/toolkt/reactive/event_stream/vertices/StatelessEventStreamVertex.kt
@@ -3,7 +3,7 @@ package dev.toolkt.reactive.event_stream.vertices
 import dev.toolkt.reactive.Transaction
 
 abstract class StatelessEventStreamVertex<ValueT> : PropagativeEventStreamVertex<ValueT>() {
-    final override fun stabilizeState(
+    final override fun stabilize(
         stabilizationContext: Transaction.StabilizationContext,
         message: EventStreamVertex.Occurrence<ValueT>?,
     ) {
