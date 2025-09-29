c4a6869913acd227ec5bace983dc8f125edc5948
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-09-29 17:52:48 +0200

    Add combo tests for some `EventStream` operators

---
+++ reactive/src/commonTest/kotlin/dev/toolkt/reactive/cell/Cell_divert_combo_tests.kt
@@ -0,0 +1,354 @@
+package dev.toolkt.reactive.cell
+
+import dev.toolkt.reactive.MomentContext
+import dev.toolkt.reactive.cell.test_utils.ConstCellFactory
+import dev.toolkt.reactive.event_stream.EmitterEventStream
+import dev.toolkt.reactive.event_stream.emit
+import dev.toolkt.reactive.event_stream.map
+import dev.toolkt.reactive.event_stream.test_utils.OccurrenceVerificationStrategy
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+@Suppress("ClassName")
+class Cell_divert_combo_tests {
+    private fun test_initialInnerOccurrence(
+        outerConstCellFactory: ConstCellFactory,
+        occurrenceVerificationStrategy: OccurrenceVerificationStrategy,
+    ) {
+        val doOccurInner = EmitterEventStream<Unit>()
+
+        val outerCell = MomentContext.execute {
+            val initialInnerEventStream = doOccurInner.map { 20 }
+
+            outerConstCellFactory.create(initialInnerEventStream)
+        }
+
+        val divertCell = Cell.divert(outerCell)
+
+        val occurrenceVerifier = occurrenceVerificationStrategy.begin(
+            subjectEventStream = divertCell,
+        )
+
+        occurrenceVerifier.verifyOccurrencePropagated(
+            doOccur = doOccurInner,
+            expectedPropagatedEvent = 20,
+        )
+    }
+
+    private fun test_initialInnerOccurrence(
+        occurrenceVerificationStrategy: OccurrenceVerificationStrategy,
+    ) {
+        ConstCellFactory.values.forEach { outerConstCellFactory ->
+            test_initialInnerOccurrence(
+                outerConstCellFactory = outerConstCellFactory,
+                occurrenceVerificationStrategy = occurrenceVerificationStrategy,
+            )
+        }
+    }
+
+    @Test
+    fun test_initialInnerOccurrence() {
+        OccurrenceVerificationStrategy.values.forEach { occurrenceVerificationStrategy ->
+            test_initialInnerOccurrence(
+                occurrenceVerificationStrategy = occurrenceVerificationStrategy,
+            )
+        }
+    }
+
+    private fun test_outerUpdate(
+        occurrenceVerificationStrategy: OccurrenceVerificationStrategy,
+    ) {
+        val doUpdateOuter = EmitterEventStream<Unit>()
+
+        val initialInnerEventStream = EmitterEventStream<Int>()
+
+        val newInnerEventStream = EmitterEventStream<Int>()
+
+        val outerCell = MomentContext.execute {
+            Cell.define(
+                initialInnerEventStream,
+                doUpdateOuter.map { newInnerEventStream },
+            )
+        }
+
+        val divertCell = Cell.divert(outerCell)
+
+        val occurrenceVerifier = occurrenceVerificationStrategy.begin(
+            subjectEventStream = divertCell,
+        )
+
+        occurrenceVerifier.verifyOccurrenceDidNotPropagate(
+            doTrigger = doUpdateOuter,
+        )
+    }
+
+    @Test
+    fun test_outerUpdate() {
+        OccurrenceVerificationStrategy.values.forEach { occurrenceVerificationStrategy ->
+            test_outerUpdate(
+                occurrenceVerificationStrategy = occurrenceVerificationStrategy,
+            )
+        }
+    }
+
+    private fun test_outerUpdate_thenInitialInnerOccurrence(
+        occurrenceVerificationStrategy: OccurrenceVerificationStrategy,
+    ) {
+        val doUpdateOuter = EmitterEventStream<Unit>()
+
+        val doUpdateInitialInner = EmitterEventStream<Unit>()
+
+        val initialInnerEventStream = EmitterEventStream<Int>()
+
+        val newInnerEventStream = EmitterEventStream<Int>()
+
+        val outerCell = MomentContext.execute {
+            Cell.define(
+                initialValue = initialInnerEventStream,
+                newValues = doUpdateOuter.map { newInnerEventStream },
+            )
+        }
+
+        val divertCell = Cell.divert(outerCell)
+
+        val occurrenceVerifier = occurrenceVerificationStrategy.begin(
+            subjectEventStream = divertCell,
+        )
+
+        doUpdateOuter.emit()
+
+        occurrenceVerifier.verifyOccurrenceDidNotPropagate(
+            doTrigger = doUpdateInitialInner,
+        )
+    }
+
+    @Test
+    fun test_outerUpdate_thenInitialInnerOccurrence_active() {
+        OccurrenceVerificationStrategy.values.forEach { occurrenceVerificationStrategy ->
+            test_outerUpdate_thenInitialInnerOccurrence(
+                occurrenceVerificationStrategy = occurrenceVerificationStrategy,
+            )
+        }
+    }
+
+    private fun test_outerUpdate_thenNewInnerUpdate(
+        occurrenceVerificationStrategy: OccurrenceVerificationStrategy,
+    ) {
+        val doUpdateOuter = EmitterEventStream<Unit>()
+
+        val doOccurNewInner = EmitterEventStream<Unit>()
+
+        val initialInnerEventStream = EmitterEventStream<Int>()
+
+        val newInnerEventStream = doOccurNewInner.map { 21 }
+
+        val outerCell = MomentContext.execute {
+            Cell.define(
+                initialInnerEventStream,
+                doUpdateOuter.map { newInnerEventStream },
+            )
+        }
+
+        val divertCell = Cell.divert(outerCell)
+
+        val occurrenceVerifier = occurrenceVerificationStrategy.begin(
+            subjectEventStream = divertCell,
+        )
+
+        doUpdateOuter.emit()
+
+        occurrenceVerifier.verifyOccurrencePropagated(
+            doOccur = doOccurNewInner,
+            expectedPropagatedEvent = 21,
+        )
+    }
+
+    @Test
+    fun test_outerUpdate_thenNewInnerUpdate() {
+        OccurrenceVerificationStrategy.values.forEach { occurrenceVerificationStrategy ->
+            test_outerUpdate_thenNewInnerUpdate(
+                occurrenceVerificationStrategy = occurrenceVerificationStrategy,
+            )
+        }
+    }
+
+    private fun test_outerUpdate_simultaneousInitialInnerOccurrence(
+        occurrenceVerificationStrategy: OccurrenceVerificationStrategy,
+    ) {
+        val doDivert = EmitterEventStream<Unit>()
+
+        val divertCell = MomentContext.execute {
+            val initialInnerEventStream = doDivert.map { 11 }
+
+            val newInnerEventStream = EmitterEventStream<Int>()
+
+            val outerCell = Cell.define(
+                initialValue = initialInnerEventStream,
+                newValues = doDivert.map { newInnerEventStream },
+            )
+
+            Cell.divert(outerCell)
+        }
+
+        val occurrenceVerifier = occurrenceVerificationStrategy.begin(
+            subjectEventStream = divertCell,
+        )
+
+        occurrenceVerifier.verifyOccurrencePropagated(
+            doOccur = doDivert,
+            expectedPropagatedEvent = 11,
+        )
+    }
+
+    @Test
+    fun test_outerUpdate_simultaneousInitialInnerOccurrence_active() {
+        OccurrenceVerificationStrategy.values.forEach { occurrenceVerificationStrategy ->
+            test_outerUpdate_simultaneousInitialInnerOccurrence(
+                occurrenceVerificationStrategy = occurrenceVerificationStrategy,
+            )
+        }
+    }
+
+    private fun test_outerUpdate_simultaneousNewInnerUpdate(
+        occurrenceVerificationStrategy: OccurrenceVerificationStrategy,
+    ) {
+        val doDivert = EmitterEventStream<Unit>()
+
+        val divertCell = MomentContext.execute {
+            val initialInnerEventStream = EmitterEventStream<Int>()
+
+            val newInnerEventStream = doDivert.map { 21 }
+
+            val outerCell = Cell.define(
+                initialValue = initialInnerEventStream,
+                newValues = doDivert.map { newInnerEventStream },
+            )
+
+            Cell.divert(outerCell)
+        }
+
+        val occurrenceVerifier = occurrenceVerificationStrategy.begin(
+            subjectEventStream = divertCell,
+        )
+
+        occurrenceVerifier.verifyOccurrenceDidNotPropagate(
+            doTrigger = doDivert,
+        )
+    }
+
+    @Test
+    fun test_outerUpdate_simultaneousNewInnerUpdate_active() {
+        OccurrenceVerificationStrategy.values.forEach { occurrenceVerificationStrategy ->
+            test_outerUpdate_simultaneousNewInnerUpdate(
+                occurrenceVerificationStrategy = occurrenceVerificationStrategy,
+            )
+        }
+    }
+
+    private fun test_outerUpdate_simultaneousBothInnerUpdates(
+        occurrenceVerificationStrategy: OccurrenceVerificationStrategy,
+    ) {
+        val doDivert = EmitterEventStream<Unit>()
+
+        val divertCell = MomentContext.execute {
+            val initialInnerEventStream = doDivert.map { 11 }
+
+            val newInnerEventStream = doDivert.map { 21 }
+
+            val outerCell = Cell.define(
+                initialValue = initialInnerEventStream,
+                newValues = doDivert.map { newInnerEventStream },
+            )
+
+            Cell.divert(outerCell)
+        }
+
+        val occurrenceVerifier = occurrenceVerificationStrategy.begin(
+            subjectEventStream = divertCell,
+        )
+
+        occurrenceVerifier.verifyOccurrencePropagated(
+            doOccur = doDivert,
+            expectedPropagatedEvent = 11,
+        )
+    }
+
+    @Test
+    fun test_outerUpdate_simultaneousBothInnerUpdates_active() {
+        OccurrenceVerificationStrategy.values.forEach { occurrenceVerificationStrategy ->
+            test_outerUpdate_simultaneousBothInnerUpdates(
+                occurrenceVerificationStrategy = occurrenceVerificationStrategy,
+            )
+        }
+    }
+
+    private fun test_deactivation_initial(
+        occurrenceVerificationStrategy: OccurrenceVerificationStrategy,
+    ) {
+        val doTrigger = EmitterEventStream<Unit>()
+
+        val divertCell = MomentContext.execute {
+            val initialInnerEventStream = doTrigger.map { 21 }
+
+            val outerCell = Cell.define(
+                initialValue = initialInnerEventStream,
+                newValues = EmitterEventStream(),
+            )
+
+            Cell.divert(outerCell)
+        }
+
+        occurrenceVerificationStrategy.verifyDeactivation(
+            subjectEventStream = divertCell,
+            doTrigger = doTrigger,
+        )
+    }
+
+    @Ignore // FIXME: Flaky test
+    @Test
+    fun test_deactivation_initial() {
+        OccurrenceVerificationStrategy.values.forEach { occurrenceVerificationStrategy ->
+            test_deactivation_initial(
+                occurrenceVerificationStrategy = occurrenceVerificationStrategy,
+            )
+        }
+    }
+
+    private fun test_deactivation_afterOuterUpdate(
+        occurrenceVerificationStrategy: OccurrenceVerificationStrategy,
+    ) {
+        val doPrepare = EmitterEventStream<Unit>()
+
+        val doTrigger = EmitterEventStream<Unit>()
+
+        val divertCell = MomentContext.execute {
+            val initialInnerEventStream = EmitterEventStream<Int>()
+
+            val newInnerEventStream = doTrigger.map { 21 }
+
+            val outerCell = Cell.define(
+                initialValue = initialInnerEventStream,
+                newValues = doPrepare.map { newInnerEventStream },
+            )
+
+            Cell.divert(outerCell)
+        }
+
+        doPrepare.emit()
+
+        occurrenceVerificationStrategy.verifyDeactivation(
+            subjectEventStream = divertCell,
+            doTrigger = doTrigger,
+        )
+    }
+
+    @Ignore // FIXME: Flaky test
+    @Test
+    fun test_deactivation_afterOuterUpdate() {
+        OccurrenceVerificationStrategy.values.forEach { occurrenceVerificationStrategy ->
+            test_deactivation_afterOuterUpdate(
+                occurrenceVerificationStrategy = occurrenceVerificationStrategy,
+            )
+        }
+    }
+}
---
+++ reactive/src/commonTest/kotlin/dev/toolkt/reactive/event_stream/test_utils/OccurrenceVerifier.kt
@@ -0,0 +1,123 @@
+package dev.toolkt.reactive.event_stream.test_utils
+
+import dev.toolkt.reactive.cell.Cell
+import dev.toolkt.reactive.cell.MutableCell
+import dev.toolkt.reactive.event_stream.EmitterEventStream
+import dev.toolkt.reactive.event_stream.EventStream
+import dev.toolkt.reactive.event_stream.NeverEventStream
+import dev.toolkt.reactive.event_stream.emit
+import dev.toolkt.reactive.event_stream.subscribe
+import kotlin.test.assertEquals
+
+abstract class OccurrenceVerifier<EventT> {
+    companion object {
+        fun <EventT> observeDirectly(
+            subjectEventStream: EventStream<EventT>,
+        ): OccurrenceVerifier<EventT> {
+            val receivedOccurredEvents = mutableListOf<EventT>()
+
+            val subscription = subjectEventStream.subscribe { occurredEvent ->
+                receivedOccurredEvents.add(occurredEvent)
+            } ?: throw IllegalStateException("Subscription should not be null.")
+
+            return object : OccurrenceVerifier<EventT>() {
+                private val receivedOccurrenceCount: Int
+                    get() = receivedOccurredEvents.size
+
+                override fun end() {
+                    subscription.cancel()
+                }
+
+                override fun verifyOccurrencePropagated(
+                    doOccur: EmitterEventStream<Unit>,
+                    expectedPropagatedEvent: EventT,
+                ) {
+                    val previousReceivedOccurrenceCount = receivedOccurrenceCount
+
+                    doOccur.emit()
+
+                    val deltaReceivedOccurrenceCount = receivedOccurrenceCount - previousReceivedOccurrenceCount
+
+                    assertEquals(
+                        expected = 1,
+                        actual = deltaReceivedOccurrenceCount,
+                        message = "Expected a single event occurrence, but got $deltaReceivedOccurrenceCount occurrences instead."
+                    )
+
+                    val singleOccurredEvent = receivedOccurredEvents.last()
+
+                    assertEquals(
+                        expected = expectedPropagatedEvent,
+                        actual = singleOccurredEvent,
+                        message = "Expected the single update to be $singleOccurredEvent, but got $singleOccurredEvent instead."
+                    )
+                }
+
+                override fun verifyOccurrenceDidNotPropagate(
+                    doTrigger: EmitterEventStream<Unit>,
+                ) {
+                    val previousReceivedOccurrenceCount = receivedOccurrenceCount
+
+                    doTrigger.emit()
+
+                    val deltaReceivedOccurrenceCount = receivedOccurrenceCount - previousReceivedOccurrenceCount
+
+                    assertEquals(
+                        expected = 0,
+                        actual = deltaReceivedOccurrenceCount,
+                        message = "Expected no event occurrences, but got $deltaReceivedOccurrenceCount occurrences instead."
+                    )
+                }
+            }
+        }
+
+        fun <EventT> observeViaDivert(
+            subjectEventStream: EventStream<EventT>,
+        ): OccurrenceVerifier<EventT> {
+            val helperOuterCell = MutableCell(
+                initialValue = subjectEventStream,
+            )
+
+            val helperDivertEventStream = Cell.divert(helperOuterCell)
+
+            val helperOccurrenceVerifier = observeDirectly(
+                subjectEventStream = helperDivertEventStream,
+            )
+
+            return object : OccurrenceVerifier<EventT>() {
+                override fun end() {
+                    helperOuterCell.set(NeverEventStream)
+                }
+
+                override fun verifyOccurrencePropagated(
+                    doOccur: EmitterEventStream<Unit>,
+                    expectedPropagatedEvent: EventT,
+                ) {
+                    helperOccurrenceVerifier.verifyOccurrencePropagated(
+                        doOccur = doOccur,
+                        expectedPropagatedEvent = expectedPropagatedEvent,
+                    )
+                }
+
+                override fun verifyOccurrenceDidNotPropagate(
+                    doTrigger: EmitterEventStream<Unit>,
+                ) {
+                    helperOccurrenceVerifier.verifyOccurrenceDidNotPropagate(
+                        doTrigger = doTrigger,
+                    )
+                }
+            }
+        }
+    }
+
+    abstract fun verifyOccurrencePropagated(
+        doOccur: EmitterEventStream<Unit>,
+        expectedPropagatedEvent: EventT,
+    )
+
+    abstract fun verifyOccurrenceDidNotPropagate(
+        doTrigger: EmitterEventStream<Unit>,
+    )
+
+    abstract fun end()
+}
