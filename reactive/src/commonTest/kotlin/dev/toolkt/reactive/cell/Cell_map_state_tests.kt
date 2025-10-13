1b1f8dd5a33f9ab29a448d2607fa6c578df50938
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-10-13 19:34:54 +0200

    Port `map` tests to timelines

--- reactive/src/commonTest/kotlin/dev/toolkt/reactive/cell/Cell_map_state_tests.kt
+++ reactive/src/commonTest/kotlin/dev/toolkt/reactive/cell/Cell_map_state_tests.kt
@@ -1,167 +1,148 @@
 package dev.toolkt.reactive.cell
 
-import dev.toolkt.reactive.MomentContext
-import dev.toolkt.reactive.cell.test_utils.CellVerificationStrategy
-import dev.toolkt.reactive.cell.test_utils.DynamicCellFactory
-import dev.toolkt.reactive.cell.test_utils.FreezingCellFactory
+import dev.toolkt.reactive.cell.test_utils.ExpectedCellTimeline
+import dev.toolkt.reactive.cell.test_utils.ExpectedCellTimeline.ExpectedFreezingUpdate
+import dev.toolkt.reactive.cell.test_utils.ExpectedCellTimeline.ExpectedJustFreeze
+import dev.toolkt.reactive.cell.test_utils.ExpectedCellTimeline.ExpectedPlainUpdate
+import dev.toolkt.reactive.cell.test_utils.GivenCellTimeline.GivenFreezingUpdate
+import dev.toolkt.reactive.cell.test_utils.GivenCellTimeline.GivenJustFreeze
+import dev.toolkt.reactive.cell.test_utils.GivenCellTimeline.GivenPlainUpdate
 import dev.toolkt.reactive.cell.test_utils.InertCellFactory
-import dev.toolkt.reactive.event_stream.EmitterEventStream
-import dev.toolkt.reactive.event_stream.map
+import dev.toolkt.reactive.cell.test_utils.Tick
+import dev.toolkt.reactive.cell.test_utils.createDynamicCellExternally
+import dev.toolkt.reactive.cell.test_utils.testCell_immediatelyInert
+import dev.toolkt.reactive.cell.test_utils.testCell_initiallyDynamic
 import kotlin.test.Ignore
 import kotlin.test.Test
 
 @Suppress("ClassName")
 class Cell_map_state_tests {
-    private fun test_state_initial(
-        sourceCellFactory: InertCellFactory,
-        verificationStrategy: CellVerificationStrategy,
-    ) {
-        val sourceCell = sourceCellFactory.createInertExternally(10)
+    @Test
+    @Ignore // FIXME: Expected a null subscription for an inert cell
+    fun test_state_sourceInert() {
+        fun test(
+            sourceCellFactory: InertCellFactory,
+        ) = testCell_immediatelyInert(
+            setup = {
+                val sourceCell = sourceCellFactory.createInertExternally(
+                    inertValue = 10,
+                )
 
-        val mapCell = sourceCell.map {
-            it.toString()
-        }
-
-        val verifier = verificationStrategy.begin(
-            subjectCell = mapCell,
+                sourceCell.map {
+                    it.toString()
+                }
+            },
+            expectedValue = "10",
         )
 
-        verifier.verifyCurrentValue(
-            expectedCurrentValue = "10",
-        )
-    }
-
-    private fun test_state_initial(
-        verificationStrategy: CellVerificationStrategy,
-    ) {
-        InertCellFactory.values.forEach { sourceCellFactory ->
-            test_state_initial(
-                sourceCellFactory = sourceCellFactory,
-                verificationStrategy = verificationStrategy,
+        InertCellFactory.values.forEach { source1CellFactory ->
+            test(
+                sourceCellFactory = source1CellFactory,
             )
         }
     }
 
     @Test
-    fun test_state_initial_passive() {
-        test_state_initial(
-            verificationStrategy = CellVerificationStrategy.Passive,
-        )
-    }
-
-    @Ignore // FIXME: Subscription should not be null.
-    @Test
-    fun test_state_initial_active() {
-        CellVerificationStrategy.Active.values.forEach { verificationStrategy ->
-            test_state_initial(
-                verificationStrategy = verificationStrategy,
+    fun test_state_sourceDynamic_noSourceUpdates() = testCell_initiallyDynamic(
+        setup = {
+            val sourceCell = createDynamicCellExternally(
+                givenInitialValue = 10,
+                givenNotificationByTick = emptyMap(),
             )
-        }
-    }
 
-    private fun test_state_sourceUpdate(
-        sourceCellFactory: DynamicCellFactory,
-        verificationStrategy: CellVerificationStrategy,
-    ) {
-        val doUpdate = EmitterEventStream<Unit>()
-
-        val sourceCell = sourceCellFactory.createDynamicExternally(
-            initialValue = 10,
-            doUpdate = doUpdate.map { 20 },
-        )
-
-        val mapCell = sourceCell.map { it.toString() }
-
-        val verifier = verificationStrategy.begin(
-            subjectCell = mapCell,
-        )
-
-        verifier.verifyUpdates(
-            doTriggerUpdate = doUpdate,
-            expectedUpdatedValue = "20",
-        )
-    }
-
-    private fun test_state_sourceUpdate(
-        verificationStrategy: CellVerificationStrategy,
-    ) {
-        DynamicCellFactory.values.forEach { sourceCellFactory ->
-            test_state_sourceUpdate(
-                sourceCellFactory = sourceCellFactory,
-                verificationStrategy = verificationStrategy,
-            )
-        }
-    }
+            sourceCell.map {
+                it.toString()
+            }
+        },
+        expectedTimeline = ExpectedCellTimeline(
+            expectedInitialValue = "10",
+            expectedNotificationByTick = emptyMap(),
+        ),
+    )
 
     @Test
-    fun test_state_sourceUpdate_passive() {
-        test_state_sourceUpdate(
-            verificationStrategy = CellVerificationStrategy.Passive,
-        )
-    }
+    fun test_state_sourceDynamic_sourceUpdates() = testCell_initiallyDynamic(
+        setup = {
+            val sourceCell = createDynamicCellExternally(
+                givenInitialValue = 10,
+                givenNotificationByTick = mapOf(
+                    Tick(1) to GivenPlainUpdate.of(
+                        givenUpdatedValue = 20,
+                    ),
+                ),
+            )
+
+            sourceCell.map {
+                it.toString()
+            }
+        },
+        expectedTimeline = ExpectedCellTimeline(
+            expectedInitialValue = "10",
+            expectedNotificationByTick = mapOf(
+                Tick(1) to ExpectedPlainUpdate(
+                    expectedUpdatedValue = "20",
+                ),
+            ),
+        ),
+    )
 
     @Test
-    fun test_state_sourceUpdate_active() {
-        CellVerificationStrategy.Active.values.forEach { verificationStrategy ->
-            test_state_sourceUpdate(
-                verificationStrategy = verificationStrategy,
+    fun test_state_sourceDynamic_sourceJustFreezes() = testCell_initiallyDynamic(
+        setup = {
+            val sourceCell = createDynamicCellExternally(
+                givenInitialValue = 10,
+                givenNotificationByTick = mapOf(
+                    Tick(1) to GivenPlainUpdate.of(
+                        givenUpdatedValue = 20,
+                    ),
+                    Tick(2) to GivenJustFreeze,
+                ),
             )
-        }
-    }
+
+            sourceCell.map {
+                it.toString()
+            }
+        },
+        expectedTimeline = ExpectedCellTimeline(
+            expectedInitialValue = "10",
+            expectedNotificationByTick = mapOf(
+                Tick(1) to ExpectedPlainUpdate(
+                    expectedUpdatedValue = "20",
+                ),
+                Tick(2) to ExpectedJustFreeze,
+            ),
+        ),
+    )
 
     @Test
-    fun test_state_sourceUpdate_quick() {
-        test_state_sourceUpdate(
-            verificationStrategy = CellVerificationStrategy.Quick,
-        )
-    }
-
-    private fun test_state_sourceFreeze(
-        sourceCellFactory: FreezingCellFactory,
-        verificationStrategy: CellVerificationStrategy,
-    ) {
-        val doFreeze = EmitterEventStream<Unit>()
-
-        val sourceCell = MomentContext.execute {
-            sourceCellFactory.create(
-                value = 10,
-                doFreeze = doFreeze,
+    fun test_state_sourceDynamic_sourceUpdatesFreezing() = testCell_initiallyDynamic(
+        setup = {
+            val sourceCell = createDynamicCellExternally(
+                givenInitialValue = 10,
+                givenNotificationByTick = mapOf(
+                    Tick(1) to GivenPlainUpdate.of(
+                        givenUpdatedValue = 20,
+                    ),
+                    Tick(2) to GivenFreezingUpdate.of(
+                        givenUpdatedValue = 30,
+                    ),
+                ),
             )
-        }
 
-        val mapCell = sourceCell.map { it.toString() }
-
-        verificationStrategy.verifyCompleteFreeze(
-            subjectCell = mapCell,
-            doFreeze = doFreeze,
-            expectedFrozenValue = "10",
-        )
-    }
-
-    private fun test_state_sourceFreeze(
-        verificationStrategy: CellVerificationStrategy,
-    ) {
-        FreezingCellFactory.values.forEach { sourceCellFactory ->
-            test_state_sourceFreeze(
-                sourceCellFactory = sourceCellFactory,
-                verificationStrategy = verificationStrategy,
-            )
-        }
-    }
-
-    @Test
-    fun test_state_sourceFreeze_passive() {
-        test_state_sourceFreeze(
-            verificationStrategy = CellVerificationStrategy.Passive,
-        )
-    }
-
-    @Test
-    fun test_state_sourceFreeze_active() {
-        CellVerificationStrategy.Active.values.forEach { verificationStrategy ->
-            test_state_sourceFreeze(
-                verificationStrategy = verificationStrategy,
-            )
-        }
-    }
+            sourceCell.map {
+                it.toString()
+            }
+        },
+        expectedTimeline = ExpectedCellTimeline(
+            expectedInitialValue = "10",
+            expectedNotificationByTick = mapOf(
+                Tick(1) to ExpectedPlainUpdate(
+                    expectedUpdatedValue = "20",
+                ),
+                Tick(2) to ExpectedFreezingUpdate(
+                    expectedUpdatedValue = "30",
+                ),
+            ),
+        ),
+    )
 }
